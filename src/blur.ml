36c8728fb7f1fcc0a3854a9dc24530b56051127f
Andrew <agrantlv@gmail.com>
2024-07-01 15:54:00 +0000

    finished grayscale and blue_screen

--- src/blur.ml
+++ src/blur.ml
@@ -1,8 +1,17 @@
 open Core
 
-(* You need to modify this function to blur the input image
-   based on the provided radius instead of ignoring it. *)
-let transform image ~radius:_ = image
+(* You need to modify this function to blur the input image based on the
+   provided radius instead of ignoring it. *)
+let transform image ~radius =
+  Image.mapi image ~f:(fun ~x ~y _pixel ->
+    Image.mean_pixel
+      (Image.slice
+         image
+         ~x_start:(x - radius)
+         ~x_end:(x + radius)
+         ~y_start:(y - radius)
+         ~y_end:(y + radius)))
+;;
 
 let command =
   Command.basic
@@ -24,5 +33,6 @@ let command =
         let image' = transform image ~radius in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
 ;;
--- src/grayscale.ml
+++ src/grayscale.ml
@@ -1,9 +1,29 @@
 open Core
 
-(* You need to change the implementation of this function so that it does something
-   to the image instead of just leaving it untouched. *)
-let transform image =
-  image
+(* You need to change the implementation of this function so that it does
+   something to the image instead of just leaving it untouched. *)
+let transform image : Image.t =
+  Image.map image ~f:(fun pixel ->
+    match pixel with
+    | r, g, b ->
+      let avg_val = (r + g + b) / 3 in
+      avg_val, avg_val, avg_val)
+;;
+
+let%expect_test "test grayscale" =
+  let reference_image =
+    Image.load_ppm ~filename:"../images/reference-beach_portrait_gray.ppm"
+    (* Image.load_ppm ~filename:"../images/beach_portrait.ppm" *)
+  in
+  let og_image = Image.load_ppm ~filename:"../images/beach_portrait.ppm" in
+  let new_image = transform og_image in
+  let _ =
+    Image.mapi new_image ~f:(fun ~x ~y pixel ->
+      if not (Pixel.equal pixel (Image.get reference_image ~x ~y))
+      then print_s [%message (x : int) (y : int)];
+      pixel)
+  in
+  [%expect ""]
 ;;
 
 let command =
@@ -20,5 +40,6 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
 ;;
